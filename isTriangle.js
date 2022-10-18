3ded619241741cacb04bd160339b2082b908be5c
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-10-18 17:58:02 +0530

    isTriangle completed

--- isTriangle.js
+++ isTriangle.js
@@ -1,6 +1,6 @@
 const inputs = document.querySelectorAll(".angle-input");
 const isTriangleBtn = document.querySelector("#is-triangle-btn");
-const outputEl = document.querySelector("#output");
+const outputEl = document.querySelector("#is-triangle-output");
 
 isTriangleBtn.addEventListener("click", isTriangle);
 
