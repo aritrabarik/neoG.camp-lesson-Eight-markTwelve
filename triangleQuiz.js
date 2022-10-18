15f7e2fd4d60e38042592626e99f6883ec43e54f
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-10-18 17:58:35 +0530

    triangleQuiz completed

--- triangleQuiz.js
+++ triangleQuiz.js
@@ -1,24 +1,26 @@
 const quizForm = document.querySelector(".quiz-form");
 
-const submitBtn = document.querySelector("#submit-answer-btn");
+const submitBtn = document.querySelector("#submit-btn");
 
-const outputEl = document.querySelector("#output");
+const outputEl = document.querySelector(".output");
 
 const correctAnswers = ["90°", "right angled"];
 
-function calculateScore() {
+function calculateScore(e) {
+  e.preventDefault();
+  const formResults = new FormData(quizForm);
+
   let score = 0;
   let index = 0;
 
-  const formResults = new FormData(quizForm);
-
   for (let value of formResults.values()) {
-    if (value === correctAnswers[index]) score++;
-
+    if (value === correctAnswers[index]) {
+      score++;
+    }
     index++;
   }
 
-  outputEl.innerText = "Your score is: " + score;
+  outputEl.innerText = `Your score is ${score}`;
 }
 
 submitBtn.addEventListener("click", calculateScore);
